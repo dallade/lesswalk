8bea38588d30905e59e24104a659e9967287be9f
Elad Dotan <eladotan@gmail.com>
2016-11-22 03:09:17 +0200

    zip manager update

--- app/src/main/java/com/lesswalk/database/ZipManager.java
+++ app/src/main/java/com/lesswalk/database/ZipManager.java
@@ -7,6 +7,7 @@ import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 /**
@@ -14,15 +15,14 @@ import java.util.zip.ZipOutputStream;
  */
 public class ZipManager {
     private static final String TAG = "ZipManager";
-    //
     private static final int BYTE_BUFFER_SIZE = 1024;
 
-    private static void zip(String[] files, String zipFileName) {
+    private static void zip(String[] files, String zipFile) {
         try {
             byte data[] = new byte[BYTE_BUFFER_SIZE];
             int numOfReadBytes;
             BufferedInputStream bis;
-            FileOutputStream fos = new FileOutputStream(zipFileName);
+            FileOutputStream fos = new FileOutputStream(zipFile);
             ZipOutputStream zipos = new ZipOutputStream(new BufferedOutputStream(fos));
 
             for (int i = 0; i < files.length; i++) {
@@ -40,6 +40,36 @@ public class ZipManager {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void unzip(String zipFileName, String unzippedDir) {
+        mkdirIfNeeded(unzippedDir);
+        try {
+            FileInputStream fin = new FileInputStream(zipFileName);
+            ZipInputStream zipis = new ZipInputStream(fin);
+            ZipEntry ze = null;
+            while ((ze = zipis.getNextEntry()) != null) {
+                //create dir if required while unzipping
+                if (ze.isDirectory()) {
+                    mkdirIfNeeded(ze.getName());
+                } else {
+                    FileOutputStream fos = new FileOutputStream(unzippedDir + ze.getName());
+                    for (int c = zipis.read(); c != -1; c = zipis.read()) {
+                        fos.write(c);
+                    }
+                    zipis.closeEntry();
+                    fos.close();
+                }
+            }
+            zipis.close();
+        } catch (Exception e) {
+            Log.e(TAG, "unzip: e.message="+e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static void mkdirIfNeeded(String dirName) {
+        //
     }
 
 }
