815c0fc85798f01b46e9577cbcee760b410def78
elazarkin <elazarkin@gmail.com>
2017-04-25 16:45:09 +0300

    almost finish ContactsAllLastSwitcher

--- app/src/main/java/com/lesswalk/views/ContactsAllLastSwitcher.java
+++ app/src/main/java/com/lesswalk/views/ContactsAllLastSwitcher.java
@@ -6,9 +6,13 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.PorterDuff;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.lesswalk.R;
@@ -25,9 +29,11 @@ public class ContactsAllLastSwitcher extends View
         MODE_ALL
     }
 
-    private Bitmap                      recentOn = null;
-    private Bitmap                      allOn    = null;
-    private ContactAllLastSwitcherModes mode     = ContactAllLastSwitcherModes.MODE_ALL;
+    private Bitmap                      recentOn        = null;
+    private Bitmap                      allOn           = null;
+    private ContactAllLastSwitcherModes mode            = ContactAllLastSwitcherModes.MODE_ALL;
+    private RectF                       recentTouchArea = null;
+    private RectF                       allTouchArea    = null;
 
     public ContactsAllLastSwitcher(Context context, AttributeSet attrs)
     {
@@ -75,10 +81,96 @@ public class ContactsAllLastSwitcher extends View
         cwork.drawLine(work.getWidth()/2, 0, work.getWidth()/2, work.getHeight(), p);
 
         cAllOn.drawBitmap(work, w/2 - fixed_w/2, h/2 - fixed_h/2, null);
+        allTouchArea = new RectF(w*3/4 - fixed_h/2, h/2 - fixed_h/2, w*3/4 + fixed_h/2, h/2 + fixed_h/2);
         cRecentOn.drawBitmap(work, w/2 - fixed_w/2, h/2 - fixed_h/2, null);
+        recentTouchArea = new RectF(w/4 - fixed_h/2, h/2 - fixed_h/2, w/4 + fixed_h/2, h/2 + fixed_h/2);
+
+        fillColor(allOn, allOn.getWidth()*3/4, allOn.getHeight()/2, Color.WHITE);
+        fillColor(recentOn, recentOn.getWidth()/4, recentOn.getHeight()/2, Color.WHITE);
+
+        drawIconLeft(allOn, recent, work.getHeight(), Color.WHITE, Color.BLACK);
+        drawIconRigth(allOn, all, work.getHeight(), Color.BLACK, Color.WHITE);
+
+        drawIconLeft(recentOn, recent, work.getHeight(), Color.BLACK, Color.WHITE);
+        drawIconRigth(recentOn, all, work.getHeight(), Color.WHITE, Color.BLACK);
 
         all.recycle();
         recent.recycle();
+        work.recycle();
+    }
+
+    private void fillColor(Bitmap bit, int x0, int y0, int color)
+    {
+        int startColor = bit.getPixel(x0, y0);
+
+        if (color != startColor)
+        {
+            fillColor(bit, x0, y0, color, startColor);
+        }
+    }
+
+    private void fillColor(Bitmap bit, int x0, int y0, int color, int startColor)
+    {
+        Point vector[]     = new Point[bit.getWidth() * bit.getHeight()];
+        int   currentIndex = 0;
+        int   size         = 0;
+        int   X_OFFSET[]   = {-1,  0, 1, -1, 0, 1, -1, 0, 1};
+        int   Y_OFFSET[]   = {-1, -1,-1,  0, 0, 0,  1, 1, 1};
+        int   x            = 0;
+        int   y            = 0;
+
+        vector[size++] = new Point(x0, y0);
+        bit.setPixel(x, y, color);
+
+        while(currentIndex < size)
+        {
+            x = vector[currentIndex].x;
+            y = vector[currentIndex].y;
+
+            for(int i = 0; i < 8; i++)
+            {
+                x = vector[currentIndex].x + X_OFFSET[i];
+                y = vector[currentIndex].y + Y_OFFSET[i];
+
+                if(x < 0 || x >= bit.getWidth() || y < 0 || y >= bit.getHeight()) continue;
+
+                if(bit.getPixel(x, y) == startColor)
+                {
+                    vector[size++] = new Point(x, y);
+                    bit.setPixel(x, y, color);
+                }
+            }
+            currentIndex++;
+        }
+    }
+
+    private void drawIconRigth(Bitmap bit, Bitmap icon, int h, int color, int negative)
+    {
+        drawIconIntoSwitcher(bit, bit.getWidth()*3/4, bit.getHeight()/2, icon, h*2/3, color, negative);
+    }
+
+    private void drawIconLeft(Bitmap bit, Bitmap icon, int h, int color, int negative)
+    {
+        drawIconIntoSwitcher(bit, bit.getWidth()/4, bit.getHeight()/2, icon, h*2/3, color, negative);
+    }
+
+    private void drawIconIntoSwitcher(Bitmap bit, int cx, int cy, Bitmap icon, int h, int color, int negative)
+    {
+        Bitmap work = icon.createScaledBitmap(icon, h, h, false);
+
+        for(int y = 0; y < work.getHeight(); y++)
+        {
+            for (int x = 0; x < work.getWidth(); x++)
+            {
+                int alpha = (work.getPixel(x, y)>>24);
+
+                if(alpha > 0) bit.setPixel(cx - work.getWidth()/2 + x, cy - work.getHeight()/2 + y, color);
+                else bit.setPixel(cx - work.getWidth()/2 + x, cy - work.getHeight()/2 + y, negative);
+                //bit.setPixel(cx - work.getWidth()/2 + x, cy - work.getHeight()/2 + y, work.getPixel(x, y));
+                //bit.setPixel(x, y, work.getPixel(x, y));
+            }
+        }
+
         work.recycle();
     }
 
@@ -109,5 +201,37 @@ public class ContactsAllLastSwitcher extends View
         {
             c.drawBitmap(allOn, 0, 0, null);
         }
+        else if(mode == ContactAllLastSwitcherModes.MODE_RECENT)
+        {
+            c.drawBitmap(recentOn, 0, 0, null);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e)
+    {
+        if(e.getAction() == MotionEvent.ACTION_UP)
+        {
+            float x = e.getX();
+            float y = e.getY();
+
+            Log.d("elazarkin3", "" + x + "," + y + "  " + allTouchArea + "  " + recentTouchArea);
+
+            if(allTouchArea.contains(x, y))
+            {
+                mode = ContactAllLastSwitcherModes.MODE_ALL;
+                invalidate();
+            }
+            else if(recentTouchArea.contains(x, y))
+            {
+                mode = ContactAllLastSwitcherModes.MODE_RECENT;
+                invalidate();
+            }
+
+            Log.d("elazarkin3", "" + mode);
+
+            return false;
+        }
+        else return true;
     }
 }
